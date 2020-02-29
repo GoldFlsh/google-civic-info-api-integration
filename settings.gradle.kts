c290854c9cae81c4eab19addf15bac6261d5fa8e
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2020-02-29 08:15:38 -0600

    Commit the start.spring.io base package

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "google-civic-info-api-integration"
---
+++ src/main/kotlin/flsh/civic/info/GoogleCivicInfoApiIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package flsh.civic.info
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleCivicInfoApiIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoogleCivicInfoApiIntegrationApplication>(*args)
+}
