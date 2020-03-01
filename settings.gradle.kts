53e4aa8b8bec453cb825c1527f8b5b29f8d553b2
GoldFlsh <Ryan.Z.Khalil@gmail.com>
2020-02-29 23:09:13 -0600

    Remove kotlin, seems like the spqr gql library does not support it.

--- settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "google-civic-info-api-integration"
---
+++ src/main/java/flsh/civic/info/GoogleCivicInfoApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package flsh.civic.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCivicInfoApiIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCivicInfoApiIntegrationApplication.class, args);
+	}
+
+}
